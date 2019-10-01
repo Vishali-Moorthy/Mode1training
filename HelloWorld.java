a18340d6cdfa8176a2c3f8eede2f6e3df50eab09
Vishali-Moorthy <vishalimoorthy1998@gmail.com>
2019-10-01 13:41:18 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package com.hcl.java;
+
+
+/**
+ * @author coalesce
+ *
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java programming");
+		
+	}
+
+}
